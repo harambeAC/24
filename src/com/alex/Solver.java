ff472798b23792eae375b5d7591ada5f67bd8c06
s-chenalex <s-chenalex@51S50030800.it.bsd405.org>
2018-10-05 17:14:50 -0700

    commited so i can work on other computer

--- src/com/alex/Solver.java
+++ src/com/alex/Solver.java
@@ -1,5 +1,16 @@
 package com.alex;
 
 public class Solver {
-
+	public static boolean solve(int[] numbers){
+		int a = numbers[0];
+		int b = numbers[1];
+		int c = numbers[2];
+		int d = numbers[4];
+	
+		
+	}
+	
+	public static int[][] getPermutations(int[] numbers){
+		int[][] 
+	}
 }
